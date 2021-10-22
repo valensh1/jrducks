a05089e004e24443a5fd092ffab9a7a4a6f9f773
Shaun Valentine <valensh1@yahoo.com>
2021-10-21 17:20:21 -0700

    changes for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const Players = require('./Model/playersModel.js'); //!Importing players model i
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || '5000';
+const path = require('path');
 
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -67,10 +68,8 @@ app.get('/teams', async (req, res) => {
   }
 });
 
-app.get('*', (req, res) => {
-  res.send(
-    '<h1 style = "color: red">You got the wrong route you IDIOT!!!</h1>'
-  );
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(path.join(__dirname, 'public', 'index.html')));
 });
 
 app.listen(PORT, () => {
